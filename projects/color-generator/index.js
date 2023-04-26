4719a1d1ded732be6c8e02341de3ae891c1465a4
LazyMan95 <nico95.nm@gmail.com>
2023-04-26 09:56:40 +0200

    Add color-generator

---
+++ projects/color-generator/index.js
@@ -0,0 +1,46 @@
+import randomColor from 'randomcolor';
+
+//let hugeColor = randomColor.random(); //
+
+//console.log(hugeColor.color);//
+let hugeColor = randomColor({ hue: 'red', count: 18 });
+console.log(hugeColor.randomColor);
+hugeColor = randomColor.count;
+console.log('#########################');
+console.log('#########################');
+console.log('#########################');
+console.log('#########################');
+console.log(
+  '#######' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    '#######',
+);
+console.log('#######' + ' ' + ' ' + randomColor + ' ' + ' ' + '#######');
+console.log(
+  '#######' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    '#######',
+);
+console.log('#########################');
+console.log('#########################');
+console.log('#########################');
